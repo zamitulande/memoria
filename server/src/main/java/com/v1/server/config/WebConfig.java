ac604455cee5dcc907c4fba6811dd26538bfed25
zamitulande <hulmartulande@gmail.com>
2024-06-27 10:43:56 -0500

    no build failled

--- server/src/main/java/com/v1/server/config/WebConfig.java
+++ server/src/main/java/com/v1/server/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://0.0.0.0:3000");
+        config.addAllowedOrigin("http://localhost:5173");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
