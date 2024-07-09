3a515902d1e08895de9dcbaf772be7894b06bf60
zamitulande <hulmartulande@gmail.com>
2024-07-08 19:24:53 -0500

    funcinal local docker

--- server/src/main/java/com/v1/server/config/WebConfig.java
+++ server/src/main/java/com/v1/server/config/WebConfig.java
@@ -23,7 +23,8 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        //config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://0.0.0.0:3000");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
