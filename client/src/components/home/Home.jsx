ef7e4098cf18ec2a3773e6503703d443ee9997ad
zamitulande <hulmartulande@gmail.com>
2024-10-03 16:51:52 -0500

    mapa-cauca

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -9,6 +9,7 @@ import image3 from "../../assets/cards/3.jpeg";
 import image5 from "../../assets/cards/5.jpeg";
 import image6 from "../../assets/cards/6.jpeg";
 import slider1 from "../../assets/home/slider1.png"
+import mapa from "../../assets/home/mapa-cauca.png"
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -43,7 +44,6 @@ const Home = () => {
         <SwiperSlide><img src={image6} width="100%" alt="Imagen 6" /></SwiperSlide>
       </Swiper>
       <Box sx={{ position: 'relative', overflow: 'hidden' }}>
-        {/* Barra inclinada */}
         <Box
           sx={{
             position: 'absolute',
@@ -70,12 +70,47 @@ const Home = () => {
               </Typography>
               En nuestro repositorio encontrara los módulos con cada una de las temáticas y testimonios que dan vida a la memoria.
             </Typography>
-            <Link to={'repositorio'}>
-              <Button variant="contained">Ir a repositorio</Button>
-            </Link>
+            <Box display="flex" justifyContent="flex-end">
+              <Link to={'repositorio'}>
+                <Button variant="contained">Ir a repositorio</Button>
+              </Link>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={12} md={6} textAlign="center">
             <img src={slider1} width="60%" />
+          </Grid>
+        </Grid>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 500,
+            left: '-50%',
+            width: '200%',
+            height: '350px',
+            backgroundColor: 'primary.main',
+            transform: 'rotate(160deg)',
+            zIndex: 0, // para que esté detrás del contenido
+          }}
+        />
+        <Grid mt={5} container alignItems="center" spacing={2} sx={{ position: 'relative', zIndex: 1 }}>
+          <Grid item xs={12} sm={12} md={6} textAlign="center">
+            <img src={mapa} width="60%" />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} justifyContent="center">
+            <Typography fontWeight="bold" fontSize={30} borderColor="primary" borderBottom={2}>
+              Escenario de violencia en el departamento del Cauca
+            </Typography>
+            <Typography component="p">
+              El Cauca fue escenario de hechos de violencia en el marco del conflicto armado interno,
+              el sometimiento a sangre y fuego de las comunidades que lo integran por parte de grupos
+              guerrilleros y otros actores armados como las AUC y paramilitares, predominaron por décadas
+              en la región. La disputa del poder y proyecto de expansión y acumulación territorial, provocó
+              un intenso accionar como la confrontación armada entre los distintos bandos, y éstos con la
+              fuerza pública, las tomas a los poblados y cabeceras municipales, arremetidas contra los puestos
+              de policía, embocadas para reducir la presencia estatal y neutralizar la influencia del enemigo,
+              además de algunos actos delictivos como la extorsión, el secuestro, y comercialización productos
+              ilícitos (CNMH, TYAG1965-2013, 2016, pág. 28).
+            </Typography>
           </Grid>
         </Grid>
       </Box>
