7f003c7c0149d5a690abd5d9dede38fd203c02e0
zamitulande <hulmartulande@gmail.com>
2024-10-21 22:33:46 -0500

    ssl jdk

--- client/src/config/Axios.jsx
+++ client/src/config/Axios.jsx
@@ -4,7 +4,7 @@ const  api = import.meta.env.VITE_API;
 
 
 const axiosClient = axios.create({
-  baseURL: `https://${api}/api/v1`,
+  baseURL: `http://${api}/api/v1`,
   headers: {
     "Content-type": "application/json"
   }
